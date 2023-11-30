a204851a40543a69c4ee7861420894d0b19b2fc5
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-30 21:45:19 +0300

    pycodestyle complacency

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
+"""
+pascal's triangle
+"""
 
 def pascal_triangle(n):
     """
     create a pascal triangle size of n
     """
-    if n <= 0:
-        return []
-
     triangle = []
-    for i in range(n):
-        row = [1] * (i + 1)
-        if i > 1:
-            for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
-
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            triangle.append(level)
     return triangle
